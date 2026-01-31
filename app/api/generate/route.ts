864bcc6fb5c3e62ee7cc5c0a7d287085a46c602d
lxklimenko <lxklimenko@gmail.com>
2026-01-31 09:09:45 +0300

    add GET for test

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -5,3 +5,10 @@ export async function POST() {
     imageUrl: "https://picsum.photos/512",
   });
 }
+
+// добавим GET для теста
+export async function GET() {
+  return NextResponse.json({
+    imageUrl: "https://picsum.photos/512",
+  });
+}
