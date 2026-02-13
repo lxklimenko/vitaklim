76d863c973a52004c546e0708ca59a04009bf75f
lxklimenko <lxklimenko@gmail.com>
2026-02-13 14:16:43 +0300

    feat: generate from prompt modal

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,17 +1,6 @@
-// lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Отсутствуют переменные окружения для Supabase. Проверь .env.local');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true,      // Сохраняет сессию в localStorage
-    autoRefreshToken: true,    // Автоматически обновляет токен доступа
-    detectSessionInUrl: true,  // Нужно для входа по ссылке из почты/OAuth
-  },
-});
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
