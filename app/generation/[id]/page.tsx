1d2a1577198e615c7257cc0294e3566825640b99
lxklimenko <lxklimenko@gmail.com>
2026-02-24 09:01:03 +0300

    telega

--- app/generation/[id]/page.tsx
+++ app/generation/[id]/page.tsx
@@ -43,7 +43,7 @@ export default async function GenerationPage({
   console.log("SIGNED ERROR:", signedError)
 
   if (!signedData?.signedUrl) {
-    throw new Error("Signed URL not created")
+    notFound()
   }
 
   const imageUrl = signedData.signedUrl
