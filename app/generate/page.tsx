f8cd19d09ff8203ebd31845af5e52c29d2b80a66
VitaKlim <lxklimenko@gmail.com>
2026-02-28 15:08:58 +0300

    gemeni

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,43 +1,5 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { useAuth } from '@/app/hooks/useAuth';
-import { useImageGeneration } from '@/app/hooks/useImageGeneration';
-import { GenerateModal } from '@/app/components/GenerateModal';
+import GenerateCore from './GenerateCore';
 
 export default function GeneratePage() {
-  const router = useRouter();
-  const { user } = useAuth();
-
-  const {
-    generatePrompt,
-    setGeneratePrompt,
-    isGenerating,
-    modelId,
-    setModelId,
-    aspectRatio,
-    setAspectRatio,
-    referencePreview,
-    handleFileChange,
-    handleRemoveImage,
-    handleGenerate
-  } = useImageGeneration(user, () => {});
-
-  return (
-    <GenerateModal
-      isOpen={true}
-      onClose={() => router.back()}
-      generatePrompt={generatePrompt}
-      setGeneratePrompt={setGeneratePrompt}
-      isGenerating={isGenerating}
-      handleGenerate={handleGenerate}
-      modelId={modelId}
-      setModelId={setModelId}
-      aspectRatio={aspectRatio}
-      setAspectRatio={setAspectRatio}
-      referencePreview={referencePreview}
-      handleFileChange={handleFileChange}
-      handleRemoveImage={handleRemoveImage}
-    />
-  );
+  return <GenerateCore />;
 }
