3f66ff1bb72b9f58d7ac6da88c6e941d66943537
lxklimenko <lxklimenko@gmail.com>
2026-03-18 22:00:15 +0300

    admin

--- app/api/set-max-webhook/route.ts
+++ app/api/set-max-webhook/route.ts
@@ -1,3 +1,20 @@
 export async function GET() {
-  return new Response("OK");
+  try {
+    const res = await fetch("https://api.max.ru/bot/setWebhook", {
+      method: "POST",
+      headers: {
+        "Authorization": "f9LHodD0cOLMc8UCrC62G1ec2CypSZR1hYdu5-DRyPm3Er_LKh5BjR-6NnnWiQqkDeviNqkKrxBsDsa-SK4V",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        url: "https://klex.pro/api/max"
+      })
+    });
+
+    const data = await res.text();
+
+    return new Response(data);
+  } catch (e) {
+    return new Response("ERROR: " + String(e));
+  }
 }
